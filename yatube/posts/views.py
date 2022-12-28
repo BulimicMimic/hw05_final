45e317331194c50d62bf5c1a766b84716f65459c
Alexander Alatkin <usenoose@gmail.com>
2022-12-28 23:05:50 +0300

    to review 1.07

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,7 +9,6 @@ from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
                                   UpdateView,
-                                  DeleteView,
                                   )
 
 from .models import Group, Post, User, Follow, Comment
